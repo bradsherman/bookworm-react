57b4ebf0c0df060d4a6984f9a59a4fad78a24514
Brad Sherman <bsherman1096@gmail.com>
2017-10-15 02:34:14 -0400

    user can now sign up

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 
 import HomePage from "./components/pages/HomePage";
 import LoginPage from "./components/pages/LoginPage";
+import SignupPage from "./components/pages/SignupPage";
 import DashboardPage from "./components/pages/DashboardPage";
 import UserRoute from "./components/routes/UserRoute";
 import GuestRoute from "./components/routes/GuestRoute";
@@ -16,6 +17,12 @@ const App = ({ location }) => (
             path="/login"
             exact
             component={LoginPage}
+        />
+        <GuestRoute
+            location={location}
+            path="/signup"
+            exact
+            component={SignupPage}
         />
         <UserRoute
             location={location}
